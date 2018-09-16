f1d3ae11ff57244e78c317768d68828a4be82010
苏鹏翔 <869664798@qq.com>
2018-09-16 13:51:56 +0800

    添加插入用户信息

--- src/main/java/com/xzfw/app/dao/UserDao.java
+++ src/main/java/com/xzfw/app/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.xzfw.app.dao;
 
 import com.xzfw.app.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -11,12 +12,20 @@ import org.apache.ibatis.annotations.Select;
  */
 
 @Mapper
-public interface UserDao {
+public interface UserDao {  
 
     /**
      * 通过手机号码查询用户信息
      */
     @Select("SELECT * FROM T_USER WHRER iphone = #{iphone}")
     User findByIphone(@Param("iphone") String iphone);
+
+    /**
+     * 插入用户信息
+     * @author 苏鹏翔
+     */
+    @Insert("INSERT into T_user (user_name,iphone,passWord,tsPassWord,headUrl) " +
+            "VALUES (#{user.userName},#{user.iphone},#{user.passWord},#{user.tsPassWord},#{user.headUrl});")
+    Integer insertUser(@Param("user") User user);
 
 }
